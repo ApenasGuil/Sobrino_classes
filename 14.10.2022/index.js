f09d18577440af6e71a9f1804fcabf1929e8cb4d
unisanta <unisanta@santacecilia.br>
2022-10-14 20:00:56 -0300

    expressoes

--- 14.10.2022/index.js
+++ 14.10.2022/index.js
@@ -12,3 +12,13 @@ console.log("A média do aluno 4 é " + calcularMedia(8,10));
 
 let mediaQuintoAluno = calcularMedia(6,9);
 console.log("A média do aluno 5 é " + mediaQuintoAluno);
+
+// Expressão de função
+
+let soma = function(n1 = 0, n2 = 0) {
+    return n1 + n2;
+}
+
+console.log("Soma de 8+3= " + soma(8,3));
+console.log("Soma de 1+7= " + soma(1,7));
+console.log("Soma()= " + soma());
