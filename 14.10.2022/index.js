5c47e1ec25201680e62afa6e720c6c1c10b29898
unisanta <unisanta@santacecilia.br>
2022-10-14 20:26:55 -0300

    repeticoes

--- 14.10.2022/index.js
+++ 14.10.2022/index.js
@@ -1,6 +1,6 @@
 // Função
 
-function calcularMedia(notaP1, notaP2) {
+/* function calcularMedia(notaP1, notaP2) {
     var media = (notaP1 + 2 * notaP2);
     return media;
 }
@@ -21,4 +21,34 @@ let soma = function(n1 = 0, n2 = 0) {
 
 console.log("Soma de 8+3= " + soma(8,3));
 console.log("Soma de 1+7= " + soma(1,7));
-console.log("Soma()= " + soma());
+console.log("Soma()= " + soma()); */
+
+// Controle de repetição de função
+/* 1. Apresentar na tela as seguintes sequencias envolvendo estruturas de controle de repetição.
+Atençao: usar variáveis de escopo local nos três casos. 
+
+For     1 a 10
+While   10 a 0
+do/while    pares de 2 a 20
+
+2. Utilizando uma expressão de função, indicar se um número fornecido como argumento é par (retornar 0 ou "Par") ou impar (retornar 1 ou "Ímpar").*/
+
+//for
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+//while
+let i=10;
+while(i>=-1){
+    console.log(i);
+    i--;
+}
+
+//do/while
+
+i=2;
+do {
+    console.log(i);
+    i+=2;
+} while (i<=20);
