abd7f6754ac0ac0cb92b9c72de3ea3c731ffa685
unisanta <unisanta@santacecilia.br>
2022-10-21 20:07:47 -0300

    deletar um elemento do array

--- 21.10.2022/index.js
+++ 21.10.2022/index.js
@@ -14,15 +14,33 @@ const arrayNumeros = [
     12,
 ];
 
-function locateElement(array, elemento) {
+/* function locateElement(array, elemento) {
     return array.indexOf(elemento);
-}
+} */
 
 /* console.log("Qual o índice de Ryan? "+array.indexOf("Ryan"));
 
 let x = array.indexOf("Norberto");
 console.log(x); */
 
-console.log("Qual o índice da Karlla? "+ locateElement(arrayNomes, "Karlla"));
+/* console.log("Qual o índice da Karlla? "+ locateElement(arrayNomes, "Karlla"));
 
-console.log("Qual o índice do 9? "+ locateElement(arrayNumeros, 9));
+console.log("Qual o índice do 9? "+ locateElement(arrayNumeros, 9)); */
+
+// ---
+
+/* Splice - https://www.javascripttutorial.net/javascript-array-splice/#:~:text=Deleting%20elements%20using%20JavaScript%20Array's%20splice()%20method&text=The%20position%20specifies%20the%20position,that%20contains%20the%20deleted%20elements. */
+
+function deleteElement(arrayNomes, element) {
+    let pos = arrayNomes.indexOf(element);
+    if(pos != -1){
+        /* return arrayNomes.splice(pos, 1); // Retorna o elemento excluído */
+        arrayNomes.splice(pos, 1); // Retorn o resto do array SEM o elemento excluído
+        return arrayNomes;
+    } else {
+        return "Elemento não existe no array.";
+    }
+    
+}
+
+console.log("Excluir o Ryan do array: " + deleteElement(arrayNomes, "Ryan"));
