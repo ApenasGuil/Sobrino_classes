338628d730597357e39162a97a1c0b19497745c4
unisanta <unisanta@santacecilia.br>
2022-10-21 20:27:39 -0300

    deletar elementos duplicados

--- 21.10.2022/index.js
+++ 21.10.2022/index.js
@@ -1,9 +1,12 @@
 const arrayNomes = [
+    "João",
     "Norberto",
     "João",
     "Ryan",
     "William",
+    "João",
     "Karlla",
+    "João",
 ];
 
 const arrayNumeros = [
@@ -31,16 +34,27 @@ console.log("Qual o índice do 9? "+ locateElement(arrayNumeros, 9)); */
 
 /* Splice - https://www.javascripttutorial.net/javascript-array-splice/#:~:text=Deleting%20elements%20using%20JavaScript%20Array's%20splice()%20method&text=The%20position%20specifies%20the%20position,that%20contains%20the%20deleted%20elements. */
 
-function deleteElement(arrayNomes, element) {
+/* function deleteElement(arrayNomes, element) {
     let pos = arrayNomes.indexOf(element);
     if(pos != -1){
-        /* return arrayNomes.splice(pos, 1); // Retorna o elemento excluído */
+        // return arrayNomes.splice(pos, 1); // Retorna o elemento excluído
         arrayNomes.splice(pos, 1); // Retorn o resto do array SEM o elemento excluído
         return arrayNomes;
     } else {
         return "Elemento não existe no array.";
     }
     
-}
+} */
 
-console.log("Excluir o Ryan do array: " + deleteElement(arrayNomes, "Ryan"));
+/* console.log("Excluir o Ryan do array: " + deleteElement(arrayNomes, "Ryan")); */
+
+function deleteElements(arrayNomes, element) {
+    let pos = arrayNomes.indexOf(element);
+    while(pos != -1){ // Enquanto haver o elemento, apaga
+        arrayNomes.splice(pos, 1);
+        pos = arrayNomes.indexOf(element);
+    }
+    return arrayNomes;
+};
+
+console.log("Excluir todos os João do array: " + deleteElements(arrayNomes, "João"));
