07262e922613bc05c521829a1bfb88e96d40a8f7
unisanta <unisanta@santacecilia.br>
2022-10-21 21:56:52 -0300

    array de objetos

--- 21.10.2022/index2.js
+++ 21.10.2022/index2.js
@@ -15,3 +15,24 @@ console.log(typeof person.isStudent);
 person.height = 1.58; // Adiciona propriedade no array, caso não exista
 
 console.table(person);
+
+const arrayAmigos = [
+    {
+        nome: "Alexandre",
+        idade: 32,
+        eAluno: false,
+    },
+    {
+        nome: "Guerra",
+        idade: 37,
+        eAluno: false,
+    },
+    {
+        nome: "Maurício",
+        idade: 35,
+        eAluno: false,
+},
+]
+
+// Array de objetos
+console.table(arrayAmigos[1].nome.length);
