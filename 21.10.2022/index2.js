611490f850168660942bbf55792ff92d0bc71934
unisanta <unisanta@santacecilia.br>
2022-10-21 21:43:47 -0300

    adiciona propriedade no array

--- 21.10.2022/index2.js
+++ 21.10.2022/index2.js
@@ -7,3 +7,11 @@ var person = {
 }
 
 console.table(person);
+console.log(typeof person);
+console.log(typeof person.name);
+console.log(typeof person.age);
+console.log(typeof person.isStudent);
+
+person.height = 1.58; // Adiciona propriedade no array, caso não exista
+
+console.table(person);
