0ecc3a87e4c476484afc0bda057e1feab857c270
unisanta <unisanta@santacecilia.br>
2022-11-04 22:08:03 -0300

    funcoes de calcular média, eventLinsteners, bloqueios + pdf TP4

--- 04.11.2022/calcularMedia/js/media.js
+++ 04.11.2022/calcularMedia/js/media.js
@@ -13,14 +13,29 @@ function getP2() {
     return Number(p2.value); //Pega valores já convertidos
 }
 
-function calcularMedia(p1=0, p2=0) {
-    let media = (p1+2*p2)/3;
+function calcularMedia(p1 = 0, p2 = 0) {
+    let media = (p1 + 2 * p2) / 3;
     return media.toFixed(2); //Limita as casas decimais
     // return ((p1+2*p2)/3).toFixed(2); //Compartado
+};
+
+function validarNota(nota) {
+    return (nota >= 0 && nota <= 10) ? true : false;
 };
 
 function onClick() {
     saida.textContent = calcularMedia(getP1(), getP2());
 };
 
+function onChange() {
+    if (validarNota(this.value) == false){
+        this.value = "";
+        this.focus();
+    }
+};
+
 btnCalcular.addEventListener("click", onClick);
+p1.addEventListener("change", onChange);
+p2.addEventListener("change", onChange);
+
+//p2.addEventListener("input", onChangeP2); //Chama a função a cada inserção no campo de input
