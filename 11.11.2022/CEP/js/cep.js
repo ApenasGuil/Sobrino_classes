c159eff5f332c19a26d29904d4d3f87dd04fda36
unisanta <unisanta@santacecilia.br>
2022-11-11 19:36:47 -0300

    getCEP()

--- 11.11.2022/CEP/js/cep.js
+++ 11.11.2022/CEP/js/cep.js
@@ -4,8 +4,12 @@ const cep = document.getElementById("cep");
 const btnPesquisar = document.getElementById("btnPesquisar");
 const saida = document.getElementById("saida");
 
+function getCEP() {
+    return Number(cep.value);
+}
+
 function buscarDadosCEP() {
-    saida.textContent = cep.value;
+    saida.textContent = getCEP();
 }
 
 btnPesquisar.addEventListener("click", buscarDadosCEP);
