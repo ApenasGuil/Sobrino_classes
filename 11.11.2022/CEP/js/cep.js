3e17f91d7a5be1e9d08a2364b1b2cdced9e7495b
unisanta <unisanta@santacecilia.br>
2022-11-11 19:35:58 -0300

    pegar valor do input

--- 11.11.2022/CEP/js/cep.js
+++ 11.11.2022/CEP/js/cep.js
@@ -3,3 +3,9 @@
 const cep = document.getElementById("cep");
 const btnPesquisar = document.getElementById("btnPesquisar");
 const saida = document.getElementById("saida");
+
+function buscarDadosCEP() {
+    saida.textContent = cep.value;
+}
+
+btnPesquisar.addEventListener("click", buscarDadosCEP);
